f76b05269089109d49eef87c50dca2f686bdfa1d
Hitesh Jain <hiteshjainhd@gmail.com>
2023-09-21 09:32:15 +0530

    handled state check

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import(
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+	if portString == ""{
+		log.Fatal("Port is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET","POST","PUT","DELETE","OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handleReadiness)
+	v1Router.Get("/err", handleErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: 	router,
+		Addr: 		":" + portString,
+	}
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
